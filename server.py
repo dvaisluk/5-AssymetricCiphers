0f743ee8e91dc4352d57274dec7923bf254eabf9
dvaisluk <70915114+dvaisluk@users.noreply.github.com>
2021-12-20 00:43:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,90 +1,128 @@
-import socket, random, threading, csv
+import socket, csv, random
 
-def encrypt(k, m):
-    return ''.join(map(chr, [(x + k) % 65536 for x in map(ord, m)]))
+def get_part_key(key_publ_1, key_prim, key_publ_2):
+    key_part_2 = key_publ_1 ** key_prim % key_publ_2
+    return key_part_2
 
-def decrypt(k, c):
-    return ''.join(map(chr, [(x - k) % 65536 for x in map(ord, c)]))
+def get_full_key(key_part_1, key_prim, key_publ_2):
+    key_full = key_part_1 ** key_prim % key_publ_2
+    return key_full
 
-def listening(sock):
-    global private_key
-    while True:
-        msg = sock.recv(1024).decode()
-        msg = decrypt(private_key, msg)
-        print(msg)
+def encoding (msg, key):
+    lst = list(msg)
+    for i in range(len(lst)):
+        variab = ord(lst[i])+key
+        variab = chr(variab)
+        lst[i] = variab
 
-def read_keys():
-    global addr
-    with open("server_key.csv", "r", newline = "") as keyfile:
-        reader = csv.reader(keyfile, delimiter = ";")
-        for row in reader:
-            if row[0] == addr[0]:
-                return row[1:] #a,g,p,A,B,H
-        else:
-            raise FileNotFoundError
+    return(''.join(lst))
 
-def get_keys():
-    global conn, addr
-    try:
-        keys = read_keys()
-    except FileNotFoundError:
-        a, g, p = [random.randint(100,999) for _ in range(3)]
-        my_a = pow(g, a) % p
-        conn.send(f"{g}|{p}|{my_a}".encode())
-        cli_b = int(conn.recv(1024).decode())
-        private = pow(cli_b, a) % p
-        keys = [a, g, p, my_a, cli_b, private]
-        with open("server_key.csv", "w", newline = "") as keyfile:
-            writer = csv.writer(keyfile, delimiter = ";")
-            writer.writerow((addr[0], *keys))
-    else:
-        keys = [int(item) for item in keys]
-        conn.send(f"{keys[1]}|{keys[2]}|{keys[3]}".encode())
-        cli_b = int(conn.recv(1024).decode())
-    return keys[5], cli_b
+def decoding (msg, key):
+    lst = list(msg)
+    for i in range(len(lst)):
+        variab = ord(lst[i]) - key
+        variab = chr(variab)
+        lst[i] = variab
+    return(''.join(lst))
 
+def generate(flagg, key_prim, key_publ_2):
+    global flag
+    while flagg<3:
+        flagg += 1
+        if flagg == 1:
+                key_publ_1 = int(conn.recv(1024))
+                if checking(key_publ_1):
+                    msg = str(key_publ_2)
+                    conn.send(msg.encode())
+                else:
+                    print("Данный ключ некорректный")
+                    flag = False
+                    break
+        if flagg == 2:
+                key_part_1 = int(conn.recv(1024))
+                key_part_2 = get_part_key(key_publ_1, key_prim, key_publ_2)
+                msg = str(key_part_2)
+                conn.send(msg.encode())
+        if flagg == 3:
+                key_full_1 = int(conn.recv(1024))
+                key_full_2 = get_full_key(key_part_1, key_prim, key_publ_2)
+                msg = str(key_full_2)
+                conn.send(msg.encode())
+                print(key_full_1)
+                with open ('server_keys.txt','w') as f: #список разрешенных ключей
+                    f.write(str(key_full_2))
+    return key_full_2
+    
+def send_recv(conn, key_full_2):
+    msg = conn.recv(1024).decode()
+    decod_msg = decoding(msg,key_full_2)
+    print('Сообщение клиена: ', decod_msg)
+    msg = input('Сообщение: ')
+    encod_msg = encoding(msg, key_full_2)
+    conn.send(encod_msg.encode())
+    return encod_msg
 
-def check_permission(cli_b):
-    with open("allowed.csv", "r", newline = "") as keyfile:
-        reader = csv.reader(keyfile, delimiter = ";")
-        for row in reader:
-            if int(row[0]) == cli_b:
-                return True
-        else:
-            return False
+def port2(conn, key_full_2, port):
+    msg = conn.recv(1024).decode()
+    decod_msg = decoding(msg,key_full_2)
+    print('Сообщение клиента: ', decod_msg)
+    msg_port = str(port)
+    print('Сообщение: ', msg_port)
+    encod_msg = encoding(msg_port, key_full_2)
+    conn.send(encod_msg.encode())
+   
+def checking(key_publ_1):
+    i = False
+    with open ('key_list.csv', 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            print(line[0])
+            if line[0] == str(key_publ_1): # публичный ключ
+                i = True
+    return i
 
-def create_socket(port = 10101):
+def bind(host_str):
     sock = socket.socket()
-    sock.setblocking(True)
-    sock.bind(('', port))
-    print(f"Socket binded at port {port}")
-    sock.listen(0)
-    conn, addr = sock.accept()
-    return sock, conn, addr
+    for prt in range(1024,65536):
+        try:
+            sock.bind((host_str, prt))
+            flg = prt
+            sock.close()
+            return flg
+        except socket.error:
+            pass
 
-def messaging_port():
-    global sock, conn, private_key
-    port = random.randint(1024,65535)
-    conn.send(encrypt(private_key, str(port)).encode())
+flag = True
+
+print('Сервер запущен')
+port = bind('localhost')
+print('Порт ',1024)
+sock = socket.socket()
+sock.bind(('', 9090))
+print('Соединение')
+sock.listen(3)
+conn, addr = sock.accept()
+print('Пользователь с адресом: ',addr[0],'присоединился')
+try:
+    with open ('server_keys.txt','r') as f:
+        for line in f:
+            key_full_2 = int(line);
+except:
+    key_publ_2 = 151
+    key_prim = 157
+    flagg=0
+    msg = ''
+    key_full_2 = generate(flagg, key_prim, key_publ_2)
+    
+if flag:
+    port2(conn, key_full_2, port)
     sock.close()
-    return create_socket(port)
-
-
-
-sock, conn, addr = create_socket()
-private_key, client_b = get_keys() 
-
-
-if check_permission(client_b):
-# if True:
-    sock, conn, addr = messaging_port()
-    threading.Thread(target = listening, args = (conn, ), daemon = True).start() 
+    sock = socket.socket()
+    sock.bind(('localhost',int(port)))
+    sock.listen(1)
+    conn, addr = sock.accept()
     while True:
-        cmd = input()
-        if cmd == "stop":
-            break
-        cmd = encrypt(private_key, cmd)
-        conn.send(cmd.encode())
-else:
-    print("Неизвестный сертификат клиента")
+        send_recv(conn, key_full_2)
+    
 sock.close()
+print('Сервер остановлен')
